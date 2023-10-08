c1aa5da2c6262ac783d50f3f555c76a5324b32ba
Eduardo Nunes Alves <eduardonnsalves@gmail.com>
2023-10-08 18:33:33 -0300

    adicionada a funcao graf_curvas, reparada a graf_bar_par e todas agora com tratamento de excecao

--- visual_edu/visual_edu.py
+++ visual_edu/visual_edu.py
@@ -4,15 +4,15 @@ import numpy as np
 from scipy.stats import norm
 
 
-def graf_pizza(df, column):
+def graf_pizza(df, coluna, titulo):
     """
-    Toma uma coluna de um df e faz um gráfico em pizza com os dados
+    Toma uma coluna do df escolhido, cria e plota um gráfico em pizza com os dados
 
     Parâmetros
     ----------
     df : df
     
-    column : df
+    coluna : str
         Coluna que será retirado os dados para fazer o gráfico em pizza    
     
     Returns
@@ -21,29 +21,35 @@ def graf_pizza(df, column):
 
     """
 
-    # Verifica se a coluna especificada está presente no df
-    if column not in df.columns:
-        print(f"A coluna '{column}' não foi encontrada no df.")
-        return
-    
-    values = df[column].value_counts()
+    if not all([df, coluna, titulo]):
+        raise ValueError("Todos os parâmetros são obrigatórios.")
 
-    plt.figure(figsize=(8, 8))
-    plt.pie(values, labels=values.index, autopct='%1.1f%%', startangle=140)
-    plt.title(f'Gráfico de Pizza para a Coluna {column}')
-    plt.show()
+    try:
+        # Verifica se a coluna especificada está presente no DataFrame
+        if coluna not in df.columns:
+            raise KeyError(f"A coluna '{coluna}' não foi encontrada no DataFrame.")
+
+        values = df[coluna].value_counts()
+
+        plt.figure(figsize=(8, 8))
+        plt.pie(values, labels=values.index, autopct='%1.1f%%', startangle=140)
+        plt.title(titulo)
+        plt.show()
+
+    except Exception as e:
+        print(f"Erro: {str(e)}")
 
 
 
-def graf_hist_desvpad(df, coluna):
+def graf_hist_desvpad(df, coluna, titulo):
     """
-    Faz um histograma com a curva de distribuiçao normal de uma coluna escolhida de um dataframe
+    Toma uma coluna do df escolhido, cria e plota um histograma com a curva de distribuição normal
 
     Parâmetros
     ----------
-    df : dataframe
+    df : df
     
-    column : df
+    coluna : str
         Coluna que será retirado os dados para fazer o histograma e a curva de distribuição normal
     
     Returns
@@ -52,23 +58,125 @@ def graf_hist_desvpad(df, coluna):
 
     """
 
-    # Verifica se a coluna especificada está presente no df
-    if coluna not in df.columns:
-        print(f"A coluna '{coluna}' não foi encontrada no df.")
-        return
+    if not all([df, coluna, titulo]):
+        raise ValueError("Todos os parâmetros são obrigatórios.")
 
-    data = df[coluna]
+    try:
+        # Verifica se a coluna especificada está presente no DataFrame
+        if coluna not in df.columns:
+            raise KeyError(f"A coluna '{coluna}' não foi encontrada no DataFrame.")
 
-    mean, std = data.mean(), data.std()
+        data = df[coluna]
 
-    plt.hist(data, bins=20, density=True, alpha=0.6, color='g')
+        mean, std = data.mean(), data.std()
 
-    xmin, xmax = plt.xlim()
-    x = np.linspace(xmin, xmax, 100)
-    p = norm.pdf(x, mean, std)
-    plt.plot(x, p, 'k', linewidth=2)
+        plt.hist(data, bins=20, density=True, alpha=0.6, color='g')
 
-    title = f'Distribuição Normal para a Coluna {coluna}'
-    plt.title(title)
-    plt.show()
+        xmin, xmax = plt.xlim()
+        x = np.linspace(xmin, xmax, 100)
+        p = norm.pdf(x, mean, std)
+        plt.plot(x, p, 'k', linewidth=2)
+
+        plt.title(titulo)
+        plt.show()
+
+    except Exception as e:
+        print(f"Erro: {str(e)}")
+
+
+def graf_bar_par(df, coluna_x, colunas_y, title, xlabel, ylabel):
+    """
+    Toma um dataframe e uma quantidade de colunas quista, cria e plota um grafico em barras com os dados das colunas escolhidas
+
+    Parâmetros
+    ----------
+    df : dataframe
+    
+    coluna_x : str
+        Coluna que dita os objetos analisados
+    
+    colunas_y : list
+        Lista com o nome das colunas do dataframe
+    
+    title : str
+    
+    xlabel : str
+
+    ylabel : str
+    
+    Returns
+    -------
+    None.
+
+    """    
+
+    if not all([coluna_x, colunas_y, title, xlabel, ylabel]):
+        raise ValueError("Todos os parâmetros são obrigatórios.")
+
+    try:
+        categorias = df[coluna_x]
+        bar_width = 0.35
+        posicoes = np.arange(len(categorias))
+        plt.figure(figsize=(10, 6))
+
+        for i, y_column in enumerate(colunas_y):
+            offset = i * bar_width
+            plt.bar(posicoes + offset, df[y_column], width=bar_width, label=y_column)
+
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.xticks(posicoes + bar_width * (len(colunas_y) - 1) / 2, categorias)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    except KeyError as e:
+        raise KeyError(f"A coluna {str(e)} não existe no DataFrame.")
+
+
+def graf_curvas(dataframe, coluna_x, coluna_y, title, xlabel, ylabel):
+    """
+    Toma um dataframe e uma quantidade de colunas quista, cria e plota as curvas com os valores dados pelas colunas
+
+    Parâmetros
+    ----------
+    df : dataframe
+    
+    coluna_x : str
+        Coluna que dita os objetos analisados
+    
+    colunas_y : list
+        Lista com o nome das colunas do dataframe
+    
+    title : str
+    
+    xlabel : str
+
+    ylabel : str
+    
+    Returns
+    -------
+    None.
+
+    """
+
+    if not all([dataframe, coluna_x, coluna_y, title, xlabel, ylabel]):
+        raise ValueError("Todos os parâmetros são obrigatórios.")
+
+    try:
+        plt.figure(figsize=(10, 6))
+
+        for y_column in coluna_y:
+            plt.plot(dataframe[coluna_x], dataframe[y_column], label=f'{y_column}')
+
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    except KeyError as e:
+        raise KeyError(f"A coluna {str(e)} não existe no DataFrame.")
 
