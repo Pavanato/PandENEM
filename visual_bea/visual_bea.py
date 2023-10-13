1cb2f24cc1703789cf32b2aa925951e18f10f710
beatrizLucia <beatrizlucia1212@gmail.com>
2023-10-12 22:43:56 -0300

    adiciona gráfico misto

--- visual_bea/visual_bea.py
+++ visual_bea/visual_bea.py
@@ -1,7 +1,7 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def grafico_barra(df, coluna_x, coluna_y, titulo, xlabel, ylabel):
-    
     '''
         Parameters
     ----------
@@ -53,3 +53,64 @@ def grafico_barra(df, coluna_x, coluna_y, titulo, xlabel, ylabel):
 
     except Exception as e:
         print(f"Erro ao criar o gráfico de barras: {str(e)}")
+
+
+def grafico_linha_barra(df, x_col, y1_col, y2_col, titulo):
+    '''
+    Cria um gráfico que combina gráfico de linha com gráfico de barras a partir de um DataFrame.
+
+    Parameters:
+    ----------
+    df (pd.DataFrame): 
+        O DataFrame de onde os dados serão extraídos.
+    x_col (str):
+        Nome da coluna para o eixo x.
+    y1_col (str):  
+        Nome da coluna para o eixo y do gráfico de linha.
+    y2_col (str): 
+        Nome da coluna para o eixo y do gráfico de barras.
+    titulo (str): 
+        Título do gráfico.
+
+    Returns:
+    None
+
+    Example:
+    >>> data = {'Ano': [2010, 2011, 2012, 2013, 2014],
+    ...         'Vendas': [100, 120, 150, 140, 180],
+    ...         'Lucro': [20, 30, 35, 25, 40]}
+    >>> df = pd.DataFrame(data)
+    >>> x_col = 'Ano'
+    >>> y1_col = 'Vendas'
+    >>> y2_col = 'Lucro'
+    >>> titulo = 'Vendas e Lucro Anuais'
+    >>> grafico_linha_barra(df, x_col, y1_col, y2_col, titulo)  # Este comando deve exibir o gráfico
+    '''
+    try:
+        # Verifica se as colunas especificadas existem no DataFrame
+        if not all(coluna in df.columns for coluna in [x_col, y1_col, y2_col]):
+            raise ValueError("Uma ou mais colunas não foram encontradas no DataFrame.")
+
+        # Cria um gráfico de linha
+        plt.figure(figsize=(10, 5))
+        plt.plot(df[x_col], df[y1_col], marker='o', color='b', label=y1_col)
+
+        # Cria um gráfico de barras
+        plt.bar(df[x_col], df[y2_col], color='g', alpha=0.5, label=y2_col)
+
+        # Configurações do gráfico
+        plt.title(titulo)
+        plt.xlabel(x_col)
+        plt.ylabel("Valores")
+        plt.legend(loc='best')
+        plt.grid(True)
+
+        # Exibe o gráfico
+        plt.show()
+
+    except Exception as e:
+        print(f"Erro ao criar o gráfico combinado: {e}")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
