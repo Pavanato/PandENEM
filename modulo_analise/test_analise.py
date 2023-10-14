58110de300040da16800338f883636cadb28ecc0
BEATRIZ LÚCIA TEIXEIRA DE SOUZA <B51113@fgv.edu.br>
2023-10-14 10:44:59 -0300

    adiciona unittest das funções top_1000 e médias unificadas

--- modulo_analise/test_analise.py
+++ modulo_analise/test_analise.py
@@ -58,8 +58,6 @@ class TestAnalise(unittest.TestCase):
             analise.nota_1000_ano(df, [2020])
 
 # FUNÇÃO RENDA_MEDIA_PER_CAPITA_FAMILIAR
-class TestRendaMediaPerCapitaFamiliar(unittest.TestCase):
-
     def test_renda_media_per_capita(self):
         # Caso de teste com colunas corretas
         df = pd.DataFrame({'Q006': ['A', 'D', 'I'], 'Q005': [4, 3, 5]})
@@ -92,6 +90,60 @@ class TestRendaMediaPerCapitaFamiliar(unittest.TestCase):
         # Verifica se a função lança uma exceção KeyError
         with self.assertRaises(KeyError):
             analise.renda_media_per_capita_familiar(df, colunas_extras)
+
+# FUNÇÃO OBTER_TOP_1000_MAIORES_MEDIAS
+class TestFuncoes(unittest.TestCase):
+    def test_obter_top_1000_maiores_medias(self):
+        # Teste para a função obter_top_1000_maiores_medias
+        df = pd.DataFrame({'NU_NOTA_CN': [80, 90, 70, 85], 'NU_NOTA_CH': [75, 88, 92, 78], 'NU_NOTA_LC': [82, 79, 88, 85], 'NU_NOTA_MT': [87, 92, 78, 90], 'NU_NOTA_REDACAO': [80, 85, 88, 92]})
+        resultado = analise.obter_top_1000_maiores_medias(df)
+        # Verifica se o resultado possui 4 linhas
+        self.assertEqual(resultado.shape[0], 4)
+        # Verifica se a primeira linha tem uma média maior ou igual à segunda
+        self.assertTrue(resultado['media'].iloc[0] >= resultado['media'].iloc[1])
+
+
+# FUNÇÃO OBTER_TOP_1000_MENORES_MEDIAS
+    def test_obter_top_1000_menores_medias(self):
+        # Teste para a função obter_top_1000_menores_medias
+        df = pd.DataFrame({'NU_NOTA_CN': [80, 90, 70, 85], 'NU_NOTA_CH': [75, 88, 92, 78], 'NU_NOTA_LC': [82, 79, 88, 85], 'NU_NOTA_MT': [87, 92, 78, 90], 'NU_NOTA_REDACAO': [80, 85, 88, 92]})
+        resultado = analise.obter_top_1000_menores_medias(df)
+        # Verifica se o resultado possui 4 linhas
+        self.assertEqual(resultado.shape[0], 4)
+        # Verifica se a primeira linha tem uma média menor ou igual à segunda
+        self.assertTrue(resultado['media'].iloc[0] <= resultado['media'].iloc[1])
+
+
+# FUNÇÃO NOTA_UNIFICADA_POR_ESTADO
+    def test_nota_unificada_por_estado(self):
+        # Teste para a função nota_unificada_por_estado
+        data = {'CO_UF_PROVA': ['MG', 'SP', 'RJ', 'MG', 'SP', 'MG'],
+                'media': [80, 85, 90, 78, 88, 92]}
+        df = pd.DataFrame(data)
+        medias_df = analise.nota_unificada_por_estado(df)
+        # Verifica se a média do estado MG é aproximadamente 83.333333 (6 casas decimais)
+        self.assertAlmostEqual(medias_df['Nota_unificada'].iloc[0], 83.333333, places=6)
+        # Verifica se a média do estado RJ é igual a 90.000000
+        self.assertAlmostEqual(medias_df['Nota_unificada'].iloc[1], 90.000000, places=6)
+        # Verifica se a média do estado SP é aproximadamente 86.500000 (6 casas decimais)
+        self.assertAlmostEqual(medias_df['Nota_unificada'].iloc[2], 86.500000, places=6)
+        
+        
+# FUNÇÃO RENDA_UNIFICADA_POR_ESTADO
+    def test_renda_unificada_por_estado(self):
+        # Teste para a função renda_unificada_por_estado
+        df_exemplo = pd.DataFrame({
+            'CO_UF_PROVA': [1, 2, 1, 2],
+            'Renda_Per_Capita': [500, 600, 700, 800],
+            'Outra_Coluna': [10, 20, 30, 40]
+        })
+        resultado = analise.renda_unificada_por_estado(df_exemplo)
+        # Verifica se o estado 1 tem uma renda unificada de 600.0
+        self.assertEqual(resultado['CO_UF_PROVA'].iloc[0], 1)
+        self.assertEqual(resultado['Renda_unificada'].iloc[0], 600.0)
+        # Verifica se o estado 2 tem uma renda unificada de 700.0
+        self.assertEqual(resultado['CO_UF_PROVA'].iloc[1], 2)
+        self.assertEqual(resultado['Renda_unificada'].iloc[1], 700.0)
 
 if __name__ == "__main__":
     unittest.main(verbosity = 3)
